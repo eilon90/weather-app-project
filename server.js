3e36ac00074c769436df24644383a2109d3408ca
eilon90 <eilon90@gmail.com>
2020-12-06 15:43:49 +0200

    fixing problems

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const api = require('./server/routes/api');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/yourDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/weather');
 
 
 app.use('/', api);
